1242580fa5d08f2afbf537c0119862945b836778
OlgaShitova <os@3d.ru>
2019-11-19 14:48:53 +0300

    старый способ использования refs

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,50 @@
 import React from "react";
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <div className="form-container card">
-        <form className="form card-body">
-          <div className="form-group">
-            <label>Username</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter username"
-            />
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter password"
-            />
-          </div>
-          <div className="form-group">
-            <label>Repeat password</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter repeat password"
-            />
-          </div>
-          <button type="submit" className="btn btn-primary w-100">
-            Submit
-          </button>
-        </form>
-      </div>
-    );
-  }
+    onSubmit = (e) => {
+      e.preventDefault();
+    console.log(this.refs.username.value, this.refs.password.value)
+    }
+
+    render() {
+        //console.log(this);
+        return (
+            <div className="form-container card">
+                <form className="form card-body">
+                    <div className="form-group">
+                        <label>Username</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter username"
+                            ref="username"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter password"
+                            ref="password"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Repeat password</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Enter repeat password"
+                            ref="repeatPassword"
+                        />
+                    </div>
+                    <button type="submit"
+                            className="btn btn-primary w-100"
+                            onClick={this.onSubmit}>
+                        Submit
+                    </button>
+                </form>
+            </div>
+        );
+    }
 }
